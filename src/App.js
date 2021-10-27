2017d63fdac97ad3646b4a7730eefb22a2311b87
eulalaoliveira <larissa.brazil@live.com>
2021-10-27 18:06:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./styles/style.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
